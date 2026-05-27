07c586c0ceb6bccc8ba8c8365c05c1ed146333f6
ankitjangid02 <ankitj2811@gmail.com>
2026-05-27 23:24:24 +0530

    commiting all files till now

---
+++ server/server.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
+    credentials: true,
+}
+
+// Middleware
+app.use(cors(corsOptions))
+app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
