83e8908172aaea04e3635eb05453a024c5221605
ankitjangid02 <ankitj2811@gmail.com>
2026-05-28 18:56:58 +0530

    all the backend work

---
+++ server/routes/projectRoutes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { protect } from '../middlewares/auth.js';
+import { deleteProject, getProjectById, getProjectPreview, getPublished, makeRevision, rollbackToVersion, saveProjectCode } from '../controllers/projectController.js';
+
+
+const projectRouter = express.Router();
+
+projectRouter.post('/revision/:projectId', protect, makeRevision)
+projectRouter.put('/save/:projectId', protect, saveProjectCode)
+projectRouter.get('/rollback/:projectId/:versionId', protect, rollbackToVersion)
+projectRouter.delete('/:projectId', protect, deleteProject)
+projectRouter.get('/preview/:projectId', protect, getProjectPreview)
+projectRouter.get('/published', getPublished)
+projectRouter.get('/published/:projectId', getProjectById)
+
+export default projectRouter
